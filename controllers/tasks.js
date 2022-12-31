6565561931a3a95c75c7380151d275f921c8e560
robgmerrill <robgmerrill@gmail.com>
2022-12-31 12:33:24 -0800

    add the rest of the tasks routes

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,7 +1,27 @@
 const getAllTasks = (req, res) => {
-  res.send("all items from new controller");
+  res.send("get all tasks");
+};
+
+const createTask = (req, res) => {
+  res.send("create task");
+};
+
+const getSingleTask = (req, res) => {
+  res.send("get single task");
+};
+
+const updateTask = (req, res) => {
+  res.send("update single task");
+};
+
+const deleteTask = (req, res) => {
+  res.send("delete task");
 };
 
 module.exports = {
   getAllTasks,
+  createTask,
+  getSingleTask,
+  updateTask,
+  deleteTask,
 };
--- routes/tasks.js
+++ routes/tasks.js
@@ -2,10 +2,18 @@
 
 const express = require("express");
 const router = express.Router();
-const { getAllTasks } = require("../controllers/tasks");
+const {
+  getAllTasks,
+  createTask,
+  updateTask,
+  deleteTask,
+  getSingleTask,
+} = require("../controllers/tasks");
 
 // imported in this controller from ../controllers/tasks
-router.route("/").get(getAllTasks);
+router.route("/").get(getAllTasks).post(createTask);
+
+router.route("/:id").get(getSingleTask).patch(updateTask).delete(deleteTask);
 
 // This line exports the `router` object so that it can be used in other parts of the application.
 module.exports = router;
