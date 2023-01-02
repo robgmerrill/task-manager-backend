e09e3395479bbf7b96f52e6bb273e527443a9804
robgmerrill <robgmerrill@gmail.com>
2023-01-02 14:05:33 -0800

    setup basic validation

--- models/task.js
+++ models/task.js
@@ -2,8 +2,16 @@ const mongoose = require("mongoose");
 
 // This is the structure for the data
 const TaskSchema = new mongoose.Schema({
-  name: String,
-  completed: Boolean,
+  name: {
+    type: String,
+    required: [true, "Must provide a name"],
+    trim: true,
+    maxlength: [20, "Name cannot be more than 20 characters"],
+  },
+  completed: {
+    type: Boolean,
+    required: [true, "Must provide a status of true or false"],
+  },
 });
 
 // set up model - representation of the data - wrapper for a schema - interface for the database for CRUD abilities
